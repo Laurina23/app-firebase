cf16c686b18893b5bc5ff1ccf372a919bb011c14
Laurina-p <laurinapatnaik20@gmail.com>
2024-07-19 20:54:05 +0530

    Implemented phone number verification

--- app/src/main/java/com/example/app_firebase/MainActivity.kt
+++ app/src/main/java/com/example/app_firebase/MainActivity.kt
@@ -1,58 +1,171 @@
 package com.example.app_firebase
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.*
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.app_firebase.ui.theme.AppfirebaseTheme
-import com.example.app_firebase.views.LoginScreen
-import com.example.app_firebase.views.RegisterScreen
-
+import com.example.testapp.R
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+    private var storedVerificationId: String? = null
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        auth = Firebase.auth
         setContent {
             AppfirebaseTheme {
-                AppNavigation()
+                val navController = rememberNavController()
+                NavHost(navController, startDestination = "login") {
+                    composable("login") { LoginScreen(navController) }
+                    composable("hello") { HelloScreen() }
+                }
             }
         }
     }
-}
-@Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "login") {
-        composable("login") {
-            LoginScreen(
-                onNavigateToHome = { navController.navigate("home") },
-                onNavigateToRegister = { navController.navigate("register") },
-            )
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            Log.d(TAG, "onVerificationCompleted:$credential")
+            signInWithPhoneAuthCredential(credential)
         }
-        composable("register") {
-            RegisterScreen(
-                onNavigateBackToLogin = { navController.popBackStack() }
-            )
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            Log.w(TAG, "onVerificationFailed", e)
+            // Handle error
         }
-        composable("home") {
-            HomeScreen()
+
+        override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
+            Log.d(TAG, "onCodeSent:$verificationId")
+            storedVerificationId = verificationId
+            resendToken = token
         }
     }
-}
 
-@Composable
-fun HomeScreen() {
-    Text("Welcome to the Home Screen!")
-}
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(callbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AppfirebaseTheme {
-        AppNavigation()
+    private fun verifyOTP(otp: String) {
+        val credential = PhoneAuthProvider.getCredential(storedVerificationId!!, otp)
+        signInWithPhoneAuthCredential(credential)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = task.result?.user
+
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                }
+            }
+    }
+
+    @Composable
+    fun LoginScreen(navController: NavHostController) {
+        var phoneNumber by remember { mutableStateOf("") }
+        var otp by remember { mutableStateOf("") }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Card(modifier = Modifier.padding(16.dp)) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.jetpack_compose_icon_rgb),
+                        contentDescription = "Jetpack Compose Icon",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    OutlinedTextField(
+                        value = phoneNumber,
+                        onValueChange = { phoneNumber = it },
+                        label = { Text(text = "Phone Number") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = { startPhoneNumberVerification(phoneNumber) },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Send OTP")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    OutlinedTextField(
+                        value = otp,
+                        onValueChange = { otp = it },
+                        label = { Text(text = "Enter OTP") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            verifyOTP(otp)
+                            navController.navigate("hello")
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Enter OTP")
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun HelloScreen() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Hello, you are successfully logged in!")
+        }
     }
 }
 
